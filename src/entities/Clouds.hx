4ee7afdcb47a5a35b2e6f9aca7a55db7c8a69d45
Bram van der Veen <96aa48@gmail.com>
2016-01-22 14:22:26 +0100

    Made the Cloud.hx loop

--- src/entities/Clouds.hx
+++ src/entities/Clouds.hx
@@ -2,15 +2,22 @@ package entities;
 
 import com.haxepunk.Entity;
 import com.haxepunk.graphics.Image;
+import com.haxepunk.HXP;
 
 class Clouds extends Entity {
   public override function new() {
     super(200, 75);
-    var sprite:Image = new Image("graphics/clouds.png");
+    sprite = new Image("graphics/clouds.png");
     graphic = sprite;
   }
 
   public override function update() {
     this.x -= .125;
+
+    if (this.x + sprite.width < 0) {
+      this.x = HXP.width;
+    }
   }
+
+  private var sprite:Image;
 }
